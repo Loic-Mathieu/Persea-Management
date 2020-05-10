b0d96f3b3617303c5c86fa0ccfcf04765d8be94e
Loic-Mathieu <loicn.mathieu@gmail.com>
2020-05-10 20:31:29 +0200

    address dao get by id

--- SERVER/src/main/java/be/hers/info/persea/dao/address/AddressDaoImpl.java
+++ SERVER/src/main/java/be/hers/info/persea/dao/address/AddressDaoImpl.java
@@ -1,12 +1,22 @@
 package be.hers.info.persea.dao.address;
 
 import be.hers.info.persea.model.address.Address;
+import be.hers.info.persea.model.contibutor.Client;
 import org.springframework.stereotype.Component;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceContextType;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import java.util.List;
 
 @Component(value = "daoAddress")
 public class AddressDaoImpl implements AddressDao {
+    @PersistenceContext(name = "localDatabase", type = PersistenceContextType.TRANSACTION)
+    private EntityManager em;
+
     @Override
     public void addOne(Address newElement) {
 
@@ -19,7 +29,13 @@ public class AddressDaoImpl implements AddressDao {
 
     @Override
     public Address getById(long id) {
-        return null;
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Address> cq = cb.createQuery(Address.class);
+
+        Root<Address> addressRoot = cq.from(Address.class);
+        cq.where(cb.equal(addressRoot.get("id"), id));
+
+        return em.createQuery(cq).getSingleResult();
     }
 
     @Override
