46f81fd8c21e4cf517ee50a60106b666cb201083
Loic-Mathieu <loicn.mathieu@gmail.com>
2020-05-25 15:37:12 +0200

    opposition

--- SERVER/src/main/java/be/hers/info/persea/filter/contributor/OppositionFilter.java
+++ SERVER/src/main/java/be/hers/info/persea/filter/contributor/OppositionFilter.java
@@ -2,12 +2,21 @@ package be.hers.info.persea.filter.contributor;
 
 import be.hers.info.persea.filter.Filter;
 import be.hers.info.persea.model.contibutor.Opposition;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 
+@Getter
+@Setter
 public class OppositionFilter implements Filter<Opposition> {
+
+    // Pagination
+    private Integer pageNumber;
+    private Integer pageSize;
+
     @Override
     public Predicate[] doFilter(CriteriaBuilder criteriaBuilder, Root<Opposition> root) {
         return new Predicate[0];
