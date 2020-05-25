f2a2ba8f2945a9fc0e7be6c1f08d5763f531f9b7
Loic-Mathieu <loicn.mathieu@gmail.com>
2020-05-25 02:11:29 +0200

    shelf commit

--- SERVER/src/main/java/be/hers/info/persea/service/courtCase/CourtCaseService.java
+++ SERVER/src/main/java/be/hers/info/persea/service/courtCase/CourtCaseService.java
@@ -3,5 +3,18 @@ package be.hers.info.persea.service.courtCase;
 import be.hers.info.persea.request.courtCase.CreateCourtCaseRequest;
 
 public interface CourtCaseService {
+    /**
+     *
+     * @param request
+     * @return
+     */
     long createCourtCase(CreateCourtCaseRequest request);
+
+    /**
+     *
+     * @param id
+     * @param rawDate
+     * @return
+     */
+    boolean updateState(long id, String rawDate);
 }
